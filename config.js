28bdab3fa2b2bcd89af09dd00c00330d7e23370b
Sudhanshu Ganesh A <76681877+Sudhi-A@users.noreply.github.com>
2022-06-04 16:51:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/app'
+
+ export const firebaseConfig = {
+    apiKey: "AIzaSyD1kw7XDlQoDBB8pK8CLEkQAnKTBRZ-_jc",
+    authDomain: "story-telling-app-c002f.firebaseapp.com",
+    projectId: "story-telling-app-c002f",
+    storageBucket: "story-telling-app-c002f.appspot.com",
+    messagingSenderId: "511698148563",
+    appId: "1:511698148563:web:4203779934f1426f9a0826"
+  
+  
+};
+
+ //Initialize Firebase
+//const app = initializeApp(firebaseConfig);
